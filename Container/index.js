41795f43abdae8382aee7dc6babd04d2b884bfc0
IamAniruth <prakashgovind4@gmail.com>
2019-06-23 12:47:50 +0530

    2 files modified, 4 added, 1 deleted

--- Container/index.js
+++ Container/index.js
@@ -1,10 +1,15 @@
 import React,{Component} from 'react';
+import Header from './Header'
+import Footer from './Footer'
+import Home from './Home'
 
 class Home extends Component{
   render(){
     return(
       <div>
-      Home
+      <Header />
+        <Home />
+        <Footer/>
       </div>
     )
   }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Home from './Home';
+import Home from './Container/index';
 import './style.css';
 
 class App extends Component {
